5a6a1f696283b4f561cbc8e0dae7a7494a4e5b29
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-06-09 15:36:22 +0530

    Corrected Shell Script

--- dGit.sh
+++ dGit.sh
@@ -9,20 +9,22 @@ TASK=$1;
 LOG=$2;
 LOG_FOLDER=".logs";
 
-if [ TASK = "init"]; then
-    if [ ! -d "$LOG_FOLDER" ]; then
-        mkdir $PROJECTS_DIR
-    fi
+if [ $TASK = "init" ]; then
+    echo "---Sucess---"
+    mkdir $LOG_FOLDER
 fi
 
-if [ TASK = "commit"]; then
-    tar -cf ./.logs/"${log}".tar
+if [ $TASK = "commit" ]; then
+    tar -cf ./"${LOG_FOLDER}"/"${LOG}".tar .
+    echo "---Sucess---"
 fi
 
-if [ TASK = "logs"]; then
+if [ $TASK = "logs" ]; then
     ls "${LOG_FOLDER}"
+    echo "---Sucess---"
 fi
 
-if [ TASK = "checkout"]; then
-    tar -xvf .logs/"${log}".tar
+if [ $TASK = "checkout" ]; then
+    tar -xvf .logs/"${LOG}".tar
+    echo "---Sucess---"
 fi
