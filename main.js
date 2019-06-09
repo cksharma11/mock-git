3782d46646c9ad5f29a494a5af9a5e50c9e9a3e0
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-06-09 11:42:06 +0530

    Initial commits

---
+++ main.js
@@ -0,0 +1,28 @@
+const shell = require("shelljs");
+
+const execCommand = (command, log) => {
+  const commands = {
+    log: () => {
+      shell.exec(`tar -cf ./.logs/${log}.tar .`);
+      console.log("Sucess");
+    },
+    logs: () => {
+      shell.exec(`ls .logs`);
+      console.log("Sucess");
+    },
+    checkout: () => {
+      shell.exec(`tar -xvf .logs/${log}.tar`);
+    }
+  };
+
+  return commands[command];
+};
+
+const main = () => {
+  const args = process.argv.slice(2);
+  const command = args[0];
+  const log = args[1];
+  execCommand(command, log)();
+};
+
+main();
