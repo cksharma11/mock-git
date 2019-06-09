ca9e592d3651efdfb0008ae6adcfc94bd428fa1f
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-06-09 14:34:51 +0530

    Rename log to commit

--- main.js
+++ main.js
@@ -1,32 +1,31 @@
 const shell = require("shelljs");
 
-const execCommand = (command, log) => {
+const help = () => {
+  console.log(`init - initialize .logs directory`);
+  console.log(`log $message - log a commit (without space)`);
+  console.log(`logs - list logs`);
+  console.log(`checkout $commit- checkout to commit`);
+};
+
+const createCommand = (task, log) => {
   const commands = {
-    init: () => {
-      shell.exec(`mkdir .logs`);
-      console.log("initialized");
-    },
-    log: () => {
-      shell.exec(`tar -cf ./.logs/${log}.tar .`);
-      console.log("Sucess");
-    },
-    logs: () => {
-      shell.exec(`ls .logs`);
-      console.log("Sucess");
-    },
-    checkout: () => {
-      shell.exec(`tar -xvf .logs/${log}.tar`);
-    }
+    init: () => shell.exec(`mkdir .logs`),
+    commit: () => shell.exec(`tar -cf ./.logs/${log}.tar .`),
+    logs: () => shell.exec(`ls .logs`),
+    checkout: () => shell.exec(`tar -xvf .logs/${log}.tar`),
+    help: () => help
   };
 
-  return commands[command];
+  return commands[task];
 };
 
 const main = () => {
   const args = process.argv.slice(2);
-  const command = args[0];
+  const task = args[0];
   const log = args[1];
-  execCommand(command, log)();
+  const command = createCommand(task, log);
+  command();
+  console.log('Done.');
 };
 
 main();
